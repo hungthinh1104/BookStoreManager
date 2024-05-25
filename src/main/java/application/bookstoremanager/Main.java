b01a9064685fa77d6b7a2e3ccf8bd95c036b7e5f
embanhanh <dinhnhuthong59@gmail.com>
2024-05-25 22:28:21 +0700

    cap nhat bao cao ton

--- src/main/java/application/bookstoremanager/Main.java
+++ src/main/java/application/bookstoremanager/Main.java
@@ -30,6 +30,52 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
+        Connection conn = DatabaseUtil.getConnection();
+        LocalDate currentDate = LocalDate.now();
+        int month = currentDate.getMonthValue() - 1;
+        int year = currentDate.getYear();
+        boolean flag = findThangNam(month,year,conn);
+        if(!flag){
+            int idThangNam = DatabaseUtil.createThangNam(conn, month, year);
+            List<Sach> allSach = DatabaseUtil.getAllSach(conn);
+            if(!findThangNam(month-1,year,conn)){
+                for(Sach sach : allSach){
+                    if(sach.getSoLuongTon() != 0){
+                        DatabaseUtil.createBaocaoton(conn, sach.getMaSach(), 0,sach.getSoLuongTon(), idThangNam);
+                    }
+                }
+            }else{
+                for(Sach sach : allSach){
+                    if(sach.getSoLuongTon() != 0){
+                        DatabaseUtil.createBaocaoton(conn, sach.getMaSach(), getToncuoiByIdsachAndThangNam(conn,sach.getMaSach(),month-1,year),
+                                sach.getSoLuongTon(), idThangNam);
+                    }
+                }
+            }
+        }
         launch();
+    }
+
+    public static boolean findThangNam(int thang, int nam, Connection conn){
+        List<Thangnam> all = DatabaseUtil.getAllThangnam(conn);
+        boolean flag = false;
+        for(Thangnam thangnam : all) {
+            if(thang == thangnam.getThang() && nam == thangnam.getNam()){
+                flag = true;
+                return flag;
+            }
+        }
+        return flag;
+    }
+
+    public static int getToncuoiByIdsachAndThangNam(Connection conn, int idsach, int thang, int nam){
+        List<Baocaoton> all = DatabaseUtil.getAllBaocaoton(conn);
+        int tonCuoi = 0;
+        for(Baocaoton baocaoton : all) {
+            if(idsach == baocaoton.getMaSach() && thang == baocaoton.getThangNam().getThang() && nam == baocaoton.getThangNam().getNam()){
+                tonCuoi = baocaoton.getTonCuoi();
+            }
+        }
+        return tonCuoi;
     }
 }
